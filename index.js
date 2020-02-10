264e2f429b751d852677299473742bfcab6c762b
Christopher Shockley <cishockleysr@gmail.com>
2020-02-09 22:42:22 -0500

    server listening on 4000

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./api/server');
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log(`===Server Running at http://localhost:${PORT}===`);
+  console.log(`Listening at http://localhost:${PORT}...`);
 });
---
+++ resources/resource-router.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const Resources = require('./resource-model');
+
+const router = express.Router;
+
+module.exports = router;
